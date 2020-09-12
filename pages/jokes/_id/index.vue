6861251efdbe5da58d8fd8be71fa0d5c2ce1fb42
alexleybourne <alexleybourne1@gmail.com>
2020-09-12 18:44:48 +1000

    Added single joke page

---
+++ pages/jokes/_id/index.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+      {{ this.joke }}
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  head() {
+    return {
+      title: "Dad Joke",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes",
+        }
+      ]
+    }
+  },
+  data() {
+    return {
+      joke: ''
+    }
+  },
+  async created() {
+    const config = {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.get(`https://icanhazdadjoke.com/j/${this.$route.params.id}`, config);
+      this.joke = res.data.joke
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
