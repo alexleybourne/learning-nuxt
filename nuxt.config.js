7cd10487dd5b47b5113904ab93a27e7fbb704fc9
alexleybourne <alexleybourne1@gmail.com>
2020-09-12 20:17:18 +1000

    Fixed routing

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,9 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'static',
+  router: {
+    base: '/learning-nuxt/'
+  },
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
